76723495e9c2f4b8f1566dc9d0cf81dd408809e9
nasheomirro <shanenescarimoro@gmail.com>
2024-01-27 14:43:48 +0800

    dynamic routing with tags and RSS implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://main--marvelous-basbousa-bd7333.netlify.app/",
   build: {
-    inlineStylesheets: 'never'
-  }
+    inlineStylesheets: "never",
+  },
 });
