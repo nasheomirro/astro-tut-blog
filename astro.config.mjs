e7c93a976522124d3b7f85d8b7f26607c1117996
nasheomirro <shanenescarimoro@gmail.com>
2024-01-27 15:19:50 +0800

    preact island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://main--marvelous-basbousa-bd7333.netlify.app/",
   build: {
-    inlineStylesheets: "never",
+    inlineStylesheets: "never"
   },
+  integrations: [preact()]
 });
